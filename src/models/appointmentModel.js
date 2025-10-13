bc286157f5cfa2f0e47e1fa2b6474706cc97a637
Darshan Kumar <darshanjagdeshkumar321@gmail.com>
2025-10-13 15:52:44 +0500

    update the shcema of appoint ri have changed ref user to doctor and postman Bug is Dockter =>myAppointment folder is not working  and staaf folder doesnot work and you will add the autorized in health

--- src/models/appointmentModel.js
+++ src/models/appointmentModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const appointmentSchema = new mongoose.Schema({
   doctorId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "Doctor",
     required: true,
   },
   patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true }, // Patient ID
